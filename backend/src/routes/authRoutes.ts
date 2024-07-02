171979f731b53e5d076a238745987ad5ec25675d
Vincent Kioko <vincentkioko51@gmail.com>
2024-07-02 21:28:00 +0300

    Auth & Journal Routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,38 +1,9 @@
 import express from 'express';
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { AppDataSource } from '../data-source';
-import { User } from '../entity/User';
+import { register, login } from '../controllers/authController';
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-    const { username, password } = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = new User();
-    user.username = username;
-    user.password = hashedPassword;
-
-    const userRepo = AppDataSource.getRepository(User)
-    await userRepo.save(user)
-
-    res.send({ message: "User redistered successfully" });
-})
-
-router.post("/login", async (req, res) => {
-    const { username, password } = req.body
-
-    const userRepo = AppDataSource.getRepository(User)
-
-    const user = await userRepo.findOneBy({ username })
-    if(!user) return res.status(400).send({ message: "User not found!" });
-
-    const isPasswordValid = await bcrypt.compare(password, user.password)
-    if(!isPasswordValid) return res.status(400).send({ message: "Invalid Password!" });
-
-    const token = jwt.sign({ userId: user.id }, "sntaks@5876", { expiresIn: "1h" })
-
-    res.send({ token })
-})
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
