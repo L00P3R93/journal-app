7609c3403d52f9d129153ba840107d1e5c87e6c4
Vincent Kioko <vincentkioko51@gmail.com>
2024-07-02 21:23:57 +0300

    Auth and Journal Controllers

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models";
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body
+
+        //Check if user already exists
+        const existingUser = await User.findOne({ where: { username } })
+        if(existingUser) return res.status(400).send({ message: "User already exists" });
+
+        //Hash Password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Create New User
+        const user = await User.create({ username, password: hashedPassword });
+        res.send({ message: "User registered Successfully", user });
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error: ", error })
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        //Retrieve & Check User
+        const user = await User.findOne({ where: { username } });
+        if(!user) res.status(400).send({ message: "Invalid username or password!" });
+
+        //Check Password
+        const isValidPassword = await bcrypt.compare(password, user!.password);
+        if(!user) return res.status(400).send({ message: "Invalid username or password!" })
+
+        //Generate JWT Token
+        const token = jwt.sign({ userId: user.id }, 'sntaks@5876', { expiresIn: '1h' });
+        res.send({ token })
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+}
---
+++ backend/src/controllers/journalController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
+import { JournalEntry, User } from "../models";
+
+export const createJournalEntry = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { title, content, category, date } = req.body;
+        const userId = req.userId;
+
+        //Fetch User
+        const user = await User.findByPk(userId)
+        if(!user) return res.status(400).send({ message: "User Not Found" });
+
+        //Create Journal Entry
+        const journalEntry = await JournalEntry.create({
+            title,
+            content,
+            category,
+            date: new Date(date),
+            userId: user.id
+        });
+        res.send({ message: "Journal Entry Created", journalEntry })
+    } catch (error) {
+        res.status(500).send({message: 'Internal Server Error'});
+    }
+}
+
+export const getJournalEntries = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const userId = req.userId;
+        //Fetch all Journal Entries created by User.
+        const entries = await JournalEntry.findAll({ where: { userId } })
+        res.send(entries)
+    } catch (error) {
+        res.status(500).send({message: 'Internal Server Error'});
+    }
+}
